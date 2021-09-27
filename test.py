84a7e508b71ac0185a5ae54860a6c4e8a07b918c
S-Patterson2210 <sam.2068@hotmail.com>
2021-09-27 10:42:26 +1000

    Add gitignore
    Download LOTR meme

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import requests 
 import matplotlib
+import numpy
+
+
+url = 'https://i.imgflip.com/5obphi.jpg'
+r = requests.get(url, allow_redirects=True)
+
+f = open ('lotr_meme.jpg', 'wb')
+f.write(r.content)
+
+f.close()
 
