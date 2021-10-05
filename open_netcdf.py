db05d6a98faa71a8e3c466a1ec2ddfb18e11f556
S-Patterson2210 <sam.2068@hotmail.com>
2021-10-05 11:51:17 +1100

    Plotting ocean current contours

--- open_netcdf.py
+++ open_netcdf.py
@@ -6,10 +6,18 @@ ds = nc.Dataset(fn)
 
 print (ds)
 
-print(ds.__dict__['YEAR'])
+lat = ds.variables['latitude'][:]
+lon = ds.variables['longitude'][:]
+time = ds.variables['time'][:]
+depth = ds.variables['depth'][:]
+
+print(lat)
+print(lon)
+# print(time)
+# print(depth)
 
 
-
-
-
+# vars = ds.variables
+# for v in vars:
+#     print (v)
 
--- plot_netCDF_on_map.py
+++ plot_netCDF_on_map.py
@@ -1,13 +1,39 @@
 import netCDF4 as nc
-from netCDF4 import Dataset as NetCDFFile 
+# from netCDF4 import Dataset as NetCDFFile 
 import matplotlib.pyplot as plt
 import numpy as np
 import mpl_toolkits
 from mpl_toolkits.basemap import Basemap
 
-nc = NetCDFFile('oscar_vel10004')
+ds = nc.Dataset('oscar_vel10004.nc')
+vars = ds.variables
+for v in vars:
+    print (v)
 
-lat = nc.variables['float64 latitude'][:]
-lon = nc.variables['float64 longitude'][:]
-time = nc.variables['int32 time'][:]
+
+lat = ds.variables['latitude'][:]
+lon = ds.variables['longitude'][:]
+time = ds.variables['time'][:]
+depth = ds.variables['depth'][:]
+u = ds.variables['u'][:]
+
+map = Basemap(projection='merc',llcrnrlon=0.,llcrnrlat=-80.,urcrnrlon=360.,urcrnrlat=80.,resolution='c')
+
+map.drawcoastlines()
+map.drawstates()
+map.drawcountries()
+map.drawlsmask(land_color='linen', ocean_color='blue')
+
+
+lons, lats = np.meshgrid(lon,lat)
+x,y = map(lons,lats)
+
+clevs = np.arange(0,100,5)
+cs = map.contour(x,y,u[0,0,:,:],clevs,colors='red',linewidths=1)
+
+plt.show()
+
+
+
+
 
