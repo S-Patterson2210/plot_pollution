1050e0362a5ac1741095c468a702a2be8c633dc4
S-Patterson2210 <sam.2068@hotmail.com>
2021-11-09 11:26:27 +1100

    SST

--- ocean_temps/heat_map.py
+++ ocean_temps/heat_map.py
@@ -3,24 +3,16 @@ import netCDF4 as nc
 import matplotlib.pyplot as plt
 import numpy as np
 import mpl_toolkits
-from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap, maskoceans
 import math
 
 # The below line denotes which file I am pulling the data from
-ds = nc.Dataset('oscar_vel10004.nc')
-vars = ds.variables
-for v in vars:
-    print (v)
+ds = nc.Dataset('./AQUA_MODIS.20211024T025501.L2.SST.NRT.nc')
 
 
-lat = ds.variables['latitude'][:]
-lon = ds.variables['longitude'][:]
-time = ds.variables['time'][:]
-depth = ds.variables['depth'][:]
-u = ds.variables['u'][:]
-v = ds.variables['v'][:]
-um = ds.variables['um'][:]
-vm = ds.variables['vm'][:]
+lat = ds.groups['navigation_data']['latitude'][:2020]
+lon = ds.groups['navigation_data']['longitude'][:2020]
+sst = ds.groups['geophysical_data']['sst'][:2020]
 
 map = Basemap(projection='merc',llcrnrlon=0.,llcrnrlat=-80.,urcrnrlon=360.,urcrnrlat=80.,resolution='h')
 
@@ -33,23 +25,16 @@ map.drawlsmask(land_color='linen', ocean_color='blue')
 
 
 # Meshgrid tells the code to create a 2D array for the map, not neccessary for data already in 2D
-stride = 1
-lons, lats = np.meshgrid(lon[::stride],lat[::stride])
-p_u = u[0,0,::stride,::stride]
-p_v = v[0,0,::stride,::stride]
+# stride = 1
+# lons, lats = np.meshgrid(lon[::stride],lat[::stride])
+# p_u = u[0,0,::stride,::stride]
+# p_v = v[0,0,::stride,::stride]
 
-curr_mag = []
-for u_row, v_row in zip(p_u, p_v):
-    curr_row = []
-    for u_val, v_val in zip (u_row, v_row):
-        abs_val = math.sqrt(u_val*u_val+v_val*v_val) 
-        curr_row.append(abs_val)
-    curr_mag.append(curr_row)
+# x,y = map(lon,lat)
+cs = map.pcolormesh(lon,lat,sst,latlon = True)
 
-x,y = map(lons,lats)
-cs = map.pcolormesh(x,y,curr_mag)
-
-
+# print (lat)
+# print (lon)
 
 # Meshgrid tells the code to create a 2D array for the map, not neccessary for data already in 2D
 # stride = 3
--- ocean_temps/open_netcdf.py
+++ ocean_temps/open_netcdf.py
@@ -1,11 +1,20 @@
 import netCDF4 as nc
 
 
-fn = './oscar_vel10004.nc'
+fn = './AQUA_MODIS.20211024T025501.L2.SST.NRT.nc'
 ds = nc.Dataset(fn)
 
-print (ds)
-
+# print (ds)
+# print (ds.groups['geophysical_data'])
+# lon = ds.groups['navigation_data']['longitude']
+# lon = lon[:2020,:]
+# lat = ds.groups['navigation_data']['latitude']
+# lat = lat[:2020,:]
+sst = ds.groups['geophysical_data']['sst'][:]
+# sst = sst[:2020,:2020]
+# print (lat)
+# print (lon)
+print (sst)
 # lat = ds.variables['latitude'][:]
 # lon = ds.variables['longitude'][:]
 # time = ds.variables['time'][:]
